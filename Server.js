1e793a4eaa4dfa182663bbf8e08a83b970803aa9
Samuel Simogiarto <samuelsimogiarto@icloud.com>
2020-04-24 21:12:20 -0700

    update Server.js

--- Server.js
+++ Server.js
@@ -58,5 +58,5 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 app.listen((process.env.PORT || 8000)), function(){
-  console.log("Server is running on port"+process.env.PORT)
+  console.log("Server is running on port"+process.env.PORT);
 });
